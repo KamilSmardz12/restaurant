9ec5c701c3cc3f565420d3015d4dd7d09d1fa7b0
kamilsmardz <kamil.smardz12@gmail.com>
2022-10-22 14:17:59 +0200

    Add DistanceCalculatorTest

---
+++ src/main/java/pl/smardz/restaurant/calculators/distance/DistanceCalculator.java
@@ -0,0 +1,12 @@
+package pl.smardz.restaurant.calculators.distance;
+
+import pl.smardz.restaurant.dto.Distance;
+import pl.smardz.restaurant.dto.Point;
+import pl.smardz.restaurant.enums.Unit;
+
+public class DistanceCalculator {
+
+    public static Distance calculate(Point restaurantPoint, Point requestPoint, Unit unit) {
+        return null;
+    }
+}
---
+++ src/test/java/pl/smardz/restaurant/calculators/distance/DistanceCalculatorTest.java
@@ -0,0 +1,73 @@
+package pl.smardz.restaurant.calculators.distance;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.smardz.restaurant.dto.Distance;
+import pl.smardz.restaurant.dto.Point;
+import pl.smardz.restaurant.enums.Unit;
+
+import java.math.BigDecimal;
+
+public class DistanceCalculatorTest {
+
+    @Test
+    void calculateDistanceInKilometersTest() {
+        final Point domostawa = Point.builder()
+                .x(BigDecimal.valueOf(50.604918))
+                .y(BigDecimal.valueOf(22.278260))
+                .build();
+        final Point sandomierz = Point.builder()
+                .x(BigDecimal.valueOf(50.684854))
+                .y(BigDecimal.valueOf(21.737200))
+                .build();
+
+        final Distance calculatedDistance = DistanceCalculator.calculate(domostawa, sandomierz, Unit.KILOMETERS);
+        final Distance expectedDistance = Distance.builder()
+                .unit(Unit.KILOMETERS)
+                .distance(BigDecimal.valueOf(39.1707))
+                .build();
+
+        Assertions.assertEquals(expectedDistance, calculatedDistance);
+    }
+
+    @Test
+    void calculateDistanceInMilesTest() {
+        final Point domostawa = Point.builder()
+                .x(BigDecimal.valueOf(50.604918))
+                .y(BigDecimal.valueOf(22.278260))
+                .build();
+        final Point sandomierz = Point.builder()
+                .x(BigDecimal.valueOf(50.684854))
+                .y(BigDecimal.valueOf(21.737200))
+                .build();
+
+        final Distance calculatedDistance = DistanceCalculator.calculate(domostawa, sandomierz, Unit.MILES);
+        final Distance expectedDistance = Distance.builder()
+                .unit(Unit.MILES)
+                .distance(BigDecimal.valueOf(24.3395))
+                .build();
+
+        Assertions.assertEquals(expectedDistance, calculatedDistance);
+    }
+
+    @Test
+    void calculateDistanceInNauticalTest() {
+        final Point domostawa = Point.builder()
+                .x(BigDecimal.valueOf(50.604918))
+                .y(BigDecimal.valueOf(22.278260))
+                .build();
+        final Point sandomierz = Point.builder()
+                .x(BigDecimal.valueOf(50.684854))
+                .y(BigDecimal.valueOf(21.737200))
+                .build();
+
+        final Distance calculatedDistance = DistanceCalculator.calculate(domostawa, sandomierz, Unit.NAUTICAL);
+        final Distance expectedDistance = Distance.builder()
+                .unit(Unit.NAUTICAL)
+                .distance(BigDecimal.valueOf(21.1365))
+                .build();
+
+        Assertions.assertEquals(expectedDistance, calculatedDistance);
+    }
+
+}
